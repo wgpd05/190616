bf7728f7abb761e0273a253f82c9c74810373e90
Chien <stdnt25@gmail.com>
2019-06-14 23:05:43 +0800

    remove EBook folder

--- EBook/p077_express/p78_express_hello.js
+++
@@ -1,7 +0,0 @@
-var app = require('express')();
-var port = process.env.PORT;
-app.listen(port);
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
-console.log('start express server\n');
